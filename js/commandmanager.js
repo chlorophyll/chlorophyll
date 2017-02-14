21b5cf6887938050314588eec0bb7410a02e3683
Chris Williamson <c.williamso@gmail.com>
2017-02-14 00:17:50 -0800

    refactoring: move enable/disable into view object

--- js/commandmanager.js
+++ js/commandmanager.js
@@ -38,7 +38,43 @@ CommandManager = function() {
 	}
 }
 
+
+function getQsPos(panel) {
+	var x = parseInt(this._panel.style.left),
+		y = parseInt(this._panel.style.top);
+	return {x: x, y: y}
+}
+
+function enableHotkey(key, callback, upCallback) {
+	if (typeof upCallback !== 'undefined') {
+		Mousetrap.bind(key, callback, "keydown");
+		Mousetrap.bind(key, upCallback, "keyup");
+	} else {
+		Mousetrap.bind(key, callback);
+	}
+}
+
+function disableHotkey(key) {
+	Mousetrap.unbind(key);
+}
+
 /*
+ * UI view object
+ *
+ * A UI view is a set of panels, controls, and hotkeys which can be enabled and
+ * disabled as a group.  Views are tracked as a hierarchy.  Disabling a group
+ * also disables all of its descendents.
+ *
+ * The optional setup() and teardown() functions will be called when the view
+ * is enabled and disabled, respectively.
+ *
+ * View definition (viewspec):
+ * 	{
+ * 		panels: [ panel list ],
+ * 		controls: [ control list ],
+ * 		hotkeys: [ listohotkeyz ]
+ * 	}
+ *
  * Panel:
  * 	{
  *		name: "panelname",
@@ -64,26 +100,10 @@ CommandManager = function() {
  * 	{
  * 		key: "x",
  * 		callback: somefunc,
- * 		upCallback: otherfunc, <- optional
+ * 		upCallback: otherfunc, <- optional, called on release
  * 		label: "Frobnicate"
  * 	}
- *
- * View definition:
- * 	{
- * 		panels: [ panel list ],
- * 		controls: [ control list ],
- * 		hotkeys: [ listohotkeyz ]
- * 	}
  */
-
-function getQsPos(panel) {
-	var x = parseInt(this._panel.style.left),
-		y = parseInt(this._panel.style.top);
-	return {x: x, y: y}
-}
-
-// A UI view, consisting of a set of controls assigned to panels and hotkeys.
-// setup() and teardown() functions are optional
 UIView = function(manager, name, viewspec, parent, setup, teardown) {
 	this.name = name;
 	this.parent = parent;
@@ -95,6 +115,7 @@ UIView = function(manager, name, viewspec, parent, setup, teardown) {
 	this.setup = function() {};
 	this.teardown = function() {};
 	this.enabled = false;
+	this.wasEnabled = false;
 
 	if ("panels" in viewspec) this.panels = viewspec.panels;
 	if ("controls" in viewspec) this.controls = viewspec.controls;
@@ -110,17 +131,19 @@ UIView = function(manager, name, viewspec, parent, setup, teardown) {
 		// var parentPos = getQsPos(panel.parent);
 		panel.qs = QuickSettings.create(panel.x, panel.y, panel.name);
 		panel.qs.hide();
+		// Panel hidden/shown state is stored separately in the object (i.e. it
+		// doesn't always match the QuickSettings state) so that enable/disable
+		// doesn't clobber visibility state when switching between UI views.
 		panel.hidden = false;
 		manager.panels[panel.name] = panel;
 	}
 
 	for (var control of this.controls) {
-		var panel = manager.getPanel(control.panel);
+		var panel = manager.panels[control.panel];
 		// The control callback is specified separately from the rest of the
 		// control arguments - append it to the parameter list.
 		control.params.push(control.callback);
-		// The type of a control is the QuickSettings add/bind function.
-		// Apply it to the panel with the given paremeters.
+		// The type of a control is just the QuickSettings add function.
 		control.type.apply(panel.qs, control.params);
 		control.title = control.params[0];
 		panel.qs.hideControl(control.title);
@@ -128,6 +151,59 @@ UIView = function(manager, name, viewspec, parent, setup, teardown) {
 
 	if (parent)
 		parent.children.push(this);
+
+	// Show a view's panels & controls and enable its hotkeys
+	this.enable = function() {
+		if (!this.enabled) {
+			this.setup();
+			this.enabled = true;
+			this.wasEnabled = true;
+			for (var panel of this.panels) {
+				if (!panel.hidden) {
+					panel.qs.show();
+				}
+			}
+			for (var control of this.controls) {
+				manager.getPanel(control.panel).showControl(control.title);
+				if ('hotkey' in control) {
+					enableHotkey(control.hotkey, control.callback);
+				}
+			}
+			for (var hotkey of this.hotkeys) {
+				enableHotkey(hotkey.key, hotkey.callback, hotkey.upCallback);
+			}
+		}
+	}
+
+	// Hide a view's panels and disable its hotkeys
+	// Also disables all children. When the group is next re-enabled, any
+	// descendents that were enabled before this call will be re-enabled.
+	this.disable = function() {
+		var subViews = this.children;
+		for (var child of subViews) {
+			child.disable();
+		}
+
+		if (this.enabled) {
+			this.teardown();
+			this.enabled = false;
+			this.wasEnabled = true;
+			for (var panel of this.panels) {
+				panel.qs.hide();
+			}
+			for (var control of this.controls) {
+				manager.getPanel(control.panel).hideControl(control.title);
+				if ('hotkey' in control) {
+					disableHotkey(control.hotkey);
+				}
+			}
+			for (var hotkey of this.hotkeys) {
+				disableHotkey(hotkey.key);
+			}
+		} else {
+			this.wasEnabled = false;
+		}
+	}
 }
 
 UIManager = function() {
@@ -185,78 +261,28 @@ UIManager = function() {
 		delete this.views[name];
 	}
 
-	function enableHotkey(key, callback, upCallback) {
-		if (typeof upCallback !== 'undefined') {
-			Mousetrap.bind(key, callback, "keydown");
-			Mousetrap.bind(key, upCallback, "keyup");
-		} else {
-			Mousetrap.bind(key, callback);
-		}
-	}
-
-	function disableHotkey(hotkey) {
-		Mousetrap.unbind(hotkey.key);
-	}
-
-	// Show a view's panels and enable its hotkeys
+	// Enabling/disabling views by name is necessary to allow for UI views
+	// which modify their own visibility state.
 	this.enableView = function(name) {
-		// Panel hidden/shown state is stored separately in the object (i.e. it
-		// doesn't always match the QuickSettings state) so that enable/disable
-		// doesn't clobber visibility state when switching between UI views.
-		var view = this.views[name];
-		if (!view.enabled) {
-			view.setup();
-			view.enabled = true;
-			for (var panel of view.panels) {
-				if (!panel.hidden) {
-					panel.qs.show();
-				}
-			}
-			for (var control of view.controls) {
-				var title = control.params[0];
-				var panel = this.panels[control.panel];
-				panel.qs.showControl(title);
-				if ('hotkey' in control) {
-					enableHotkey(control.hotkey, control.callback);
-				}
-			}
-			for (var hotkey of view.hotkeys) {
-				enableHotkey(hotkey.key, hotkey.callback, hotkey.upCallback);
-			}
+		if (!(name in this.views)) {
+			console.error("UIManager: no such view: ", name);
+			return;
 		}
+		var view = this.views[name];
+		view.enable();
 	}
 
-	// Hide a view's panels and disable its hotkeys
-	// Also disables all children
 	this.disableView = function(name) {
+		if (!(name in this.views)) {
+			console.error("UIManager: no such view: ", name);
+			return;
+		}
 		var view = this.views[name];
-		var subViews = view.children;
-		for (var child of subViews) {
-			this.disableView(child.name);
-		}
-
-		if (view.enabled) {
-			view.teardown();
-			view.enabled = false;
-			for (var panel of view.panels) {
-				panel.qs.hide();
-			}
-			for (var control of view.controls) {
-				var title = control.params[0];
-				var panel = this.panels[control.panel];
-				panel.qs.hideControl(title);
-				if ('hotkey' in control) {
-					disableHotkey(control.hotkey);
-				}
-			}
-			for (var hotkey of view.hotkeys) {
-				disableHotkey(hotkey.key);
-			}
-		}
+		view.disable();
 	}
 
 	// Enable one view and hide all its siblings (other children of its parent)
-	this.setViewExclusive = function(name) {
+	this.enableViewExclusive = function(name) {
 		var view = this.views[name];
 		if (!view.parent) {
 			this.enableView(name);
@@ -283,14 +309,11 @@ UIManager = function() {
 	}
 
 	this.getPanel = function(panelName) {
-		return this.panels[panelName];
+		return this.panels[panelName].qs;
 	}
 }
 
 
-//		panel position is in relation to its parent
-//			attach mode: unattached/top/bottom/left/right/absolute
-//
 // for toolbox: Create toolbox as child of Model-editing-mode in init func.
 // 		tracks active thing internally. Sets colors when appropriate key/bhutan
 // 		/etc. pushed to indicate active tool.
