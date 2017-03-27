3ea288d5a98cb17df5986cb576c688c1370390a0
Ryan Pearl <rpearl@endofunctor.org>
2017-03-26 23:42:09 -0700

    Add a fuck ton more functionality nodes

--- js/patterns/nodes.js
+++ js/patterns/nodes.js
@@ -66,26 +66,104 @@ LiteGraph.registerNodeType("logic/if", IfNode);
 function EqualsNode() {
 	this.addInput('a');
 	this.addInput('b');
-	this.addOutput('result', 'boolean');
+	this.addOutput('a == b', 'boolean');
 }
 
-EqualsNode.title = 'equals'
+EqualsNode.title = 'a == b'
 
 EqualsNode.prototype.onExecute = function() {
 	var a = this.getInputData(0);
 	var b = this.getInputData(1);
 	this.setOutputData(0, a == b);
 }
-LiteGraph.registerNodeType("logic/equals", EqualsNode);
+LiteGraph.registerNodeType("logic/==", EqualsNode);
 
+function LessNode() {
+	this.addInput('a', 'number');
+	this.addInput('b', 'number');
+	this.addOutput('a < b', 'boolean');
+}
+LessNode.title = 'a < b'
+LessNode.prototype.onExecute = function() {
+	this.setOutputData(0, this.getInputData(0) < this.getInputData(1));
+}
+LiteGraph.registerNodeType("logic/<", LessNode);
 
+function LessEqualNode() {
+	this.addInput('a', 'number');
+	this.addInput('b', 'number');
+	this.addOutput('a <= b', 'boolean');
+}
+LessEqualNode.title = 'a <= b'
+LessEqualNode.prototype.onExecute = function() {
+	this.setOutputData(0, this.getInputData(0) <= this.getInputData(1));
+}
+LiteGraph.registerNodeType("logic/<=", LessEqualNode);
+
+function GreaterNode() {
+	this.addInput('a', 'number');
+	this.addInput('b', 'number');
+	this.addOutput('a > b', 'boolean');
+}
+GreaterNode.title = 'a > b'
+GreaterNode.prototype.onExecute = function() {
+	this.setOutputData(0, this.getInputData(0) > this.getInputData(1));
+}
+LiteGraph.registerNodeType("logic/>", GreaterNode);
+
+function GreaterEqualNode() {
+	this.addInput('a', 'number');
+	this.addInput('b', 'number');
+	this.addOutput('a >= b', 'boolean');
+}
+GreaterEqualNode.title = 'a >= b'
+GreaterEqualNode.prototype.onExecute = function() {
+	this.setOutputData(0, this.getInputData(0) >= this.getInputData(1));
+}
+LiteGraph.registerNodeType("logic/>=", GreaterEqualNode);
+
+function AndNode() {
+	this.addInput('a');
+	this.addInput('b');
+	this.addOutput('a && b', 'boolean');
+}
+AndNode.title = 'a && b'
+AndNode.prototype.onExecute = function() {
+	this.setOutputData(0, this.getInputData(0) && this.getInputData(1));
+}
+LiteGraph.registerNodeType("logic/&&", AndNode);
+
+function OrNode() {
+	this.addInput('a');
+	this.addInput('b');
+	this.addOutput('a || b', 'boolean');
+}
+OrNode.title = 'a || b'
+OrNode.prototype.onExecute = function() {
+	this.setOutputData(0, this.getInputData(0) || this.getInputData(1));
+}
+LiteGraph.registerNodeType("logic/||", OrNode);
+
+function NotNode() {
+	this.addInput('a');
+	this.addOutput('!a', 'boolean');
+}
+
+NotNode.title = '!a';
+
+NotNode.prototype.onExecute = function() {
+	this.setOutputData(0, !this.getInputData(0));
+}
+
+LiteGraph.registerNodeType("logic/!", NotNode);
 
 function AddNode() {
 	this.addInput('a', 'number');
 	this.addInput('b', 'number');
-	this.addOutput('result', 'number');
+	this.addOutput('a + b', 'number');
 }
-AddNode.title = 'add';
+
+AddNode.title = 'a + b';
 AddNode.prototype.onExecute = function() {
 	var a = this.getInputData(0);
 	var b = this.getInputData(1);
@@ -93,12 +171,40 @@ AddNode.prototype.onExecute = function() {
 }
 LiteGraph.registerNodeType("math/add", AddNode);
 
+function SubNode() {
+	this.addInput('a', 'number');
+	this.addInput('b', 'number');
+	this.addOutput('a - b', 'number');
+}
+
+SubNode.title = 'a - b';
+SubNode.prototype.onExecute = function() {
+	var a = this.getInputData(0);
+	var b = this.getInputData(1);
+	this.setOutputData(0, a-b);
+}
+LiteGraph.registerNodeType("math/sub", SubNode);
+
+function MulNode() {
+	this.addInput('a', 'number');
+	this.addInput('b', 'number');
+	this.addOutput('a * b', 'number');
+}
+
+MulNode.title = 'a * b';
+MulNode.prototype.onExecute = function() {
+	var a = this.getInputData(0);
+	var b = this.getInputData(1);
+	this.setOutputData(0, a*b);
+}
+LiteGraph.registerNodeType("math/mul", MulNode);
+
 function DivNode() {
 	this.addInput('a', 'number');
 	this.addInput('b', 'number');
 	this.addOutput('result', 'number');
 }
-DivNode.title = 'div';
+DivNode.title = 'a / b';
 DivNode.prototype.onExecute = function() {
 	var a = this.getInputData(0);
 	var b = this.getInputData(1);
@@ -106,3 +212,28 @@ DivNode.prototype.onExecute = function() {
 }
 LiteGraph.registerNodeType("math/div", DivNode);
 
+function AngleNode() {
+	this.addInput('theta', 'number');
+	this.addOutput('normalized', 'number');
+}
+
+AngleNode.title = 'normalizeAngle'
+AngleNode.prototype.onExecute = function() {
+	var theta = this.getInputData(0);
+	this.setOutputData(0, 255 * theta / (2*Math.PI));
+}
+
+LiteGraph.registerNodeType('polar/normalizeAngle', AngleNode);
+
+function ToUint8() {
+	this.addInput('x', 'number');
+	this.addOutput('result', 'number');
+}
+
+ToUint8.title = 'toUint8';
+
+ToUint8.prototype.onExecute = function() {
+	this.setOutputData(0, this.getInputData(0) & 0xff);
+}
+
+LiteGraph.registerNodeType('math/toUint8', ToUint8);
