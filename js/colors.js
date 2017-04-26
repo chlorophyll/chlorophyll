0b134f60811247c462cfa553a8d5a448712de1f2
Ryan Pearl <rpearl@endofunctor.org>
2017-04-25 22:49:56 -0700

    Pick a more contrasting color set that only contains bright colors

--- js/colors.js
+++ js/colors.js
@@ -1,49 +1,45 @@
-ColorPool = {
-	colors: [
-		new THREE.Color(0xf0ffff),
-		new THREE.Color(0xf5f5dc),
-		new THREE.Color(0x0000ff),
-		new THREE.Color(0xa52a2a),
-		new THREE.Color(0x00ffff),
-		new THREE.Color(0x00008b),
-		new THREE.Color(0x008b8b),
-		new THREE.Color(0xa9a9a9),
-		new THREE.Color(0x006400),
-		new THREE.Color(0xbdb76b),
-		new THREE.Color(0x8b008b),
-		new THREE.Color(0xff8c00),
-		new THREE.Color(0x9932cc),
-		new THREE.Color(0x8b0000),
-		new THREE.Color(0xe9967a),
-		new THREE.Color(0x9400d3),
-		new THREE.Color(0xff00ff),
-		new THREE.Color(0xffd700),
-		new THREE.Color(0x008000),
-		new THREE.Color(0x4b0082),
-		new THREE.Color(0xf0e68c),
-		new THREE.Color(0xadd8e6),
-		new THREE.Color(0xe0ffff),
-		new THREE.Color(0x90ee90),
-		new THREE.Color(0xd3d3d3),
-		new THREE.Color(0xffb6c1),
-		new THREE.Color(0xffffb0),
-		new THREE.Color(0x00ff00),
-		new THREE.Color(0xff00ff),
-		new THREE.Color(0x800000),
-		new THREE.Color(0x808000),
-		new THREE.Color(0xffa500),
-		new THREE.Color(0x800080),
-		new THREE.Color(0xff0000),
-		new THREE.Color(0xffff00)],
+ColorPool = (function() {
+	var colors = [
+		new THREE.Color(0x24c6ff),
+		new THREE.Color(0xffa21a),
+		new THREE.Color(0x774cfa),
+		new THREE.Color(0xb4f553),
+		new THREE.Color(0xda00a9),
+		new THREE.Color(0x00df8a),
+		new THREE.Color(0x8938a7),
+		new THREE.Color(0xb7b500),
+		new THREE.Color(0x015ad6),
+		new THREE.Color(0xcd9600),
+		new THREE.Color(0x798bff),
+		new THREE.Color(0x009d47),
+		new THREE.Color(0xff8eff),
+		new THREE.Color(0x236b1f),
+		new THREE.Color(0xb31361),
+		new THREE.Color(0x02a894),
+		new THREE.Color(0xff6c7e),
+		new THREE.Color(0x007087),
+		new THREE.Color(0xad2943),
+		new THREE.Color(0x7bbcff),
+		new THREE.Color(0x6a7c00),
+		new THREE.Color(0xd6afff),
+		new THREE.Color(0x7c6000),
+		new THREE.Color(0xff8eb4),
+		new THREE.Color(0x854875),
+	]
 
 	/* TODO make this not repeat colors
 	this.unusedColors = colors;
 
 	this.get = function() {};
 	this.recycle = function() {};
-	*/
 
-	random: function() {
-		return this.colors[Math.floor(Math.random() * this.colors.length)];
+	*/
+	var i = 0;
+
+	return {
+		random: function() {
+			i = (i + 1) % (colors.length);
+			return colors[i];
+		}
 	}
-};
+})();
