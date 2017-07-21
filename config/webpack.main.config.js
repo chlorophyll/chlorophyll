c5c66885e6842ad39e9c986aa9f1992a8b76b9b6
Chris Williamson <c.williamso@gmail.com>
2017-07-20 21:45:50 -0700

    Move electron main process files into config

--- config/webpack.main.config.js
+++ config/webpack.main.config.js
@@ -8,7 +8,7 @@ const webpack = require('webpack')
 
 let mainConfig = {
   entry: {
-    main: path.join(__dirname, '../src/main/index.js')
+    main: path.join(__dirname, './main/index.js')
   },
   externals: [
     ...Object.keys(dependencies || {})
