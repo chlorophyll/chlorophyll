a07d12cd4dfdca53d45572a9815c235072788df6
Ryan Pearl <rpearl@endofunctor.org>
2017-09-04 15:17:55 -0700

    Add a tab-set widget

---
+++ editor/src/components/widgets/tab-set/index.vue
@@ -0,0 +1,94 @@
+<template>
+<div class="tab-container">
+    <ul class="tab-list">
+        <li v-for="tab in tabs"
+            :class="{'active': current === tab}"
+            @click="selectTab(tab)">
+            {{ tab.title }}
+        </li>
+    </ul>
+    <div class="tab-content">
+        <slot />
+    </div>
+</div>
+</template>
+<script>
+import Tab from '@/components/widgets/tab-set/tab';
+
+export default {
+    name: 'tab-set',
+    data() {
+        return {
+            tabs: [],
+            current: null,
+        };
+    },
+    components: { Tab },
+
+    methods: {
+        selectTab(tab) {
+            this.current = tab;
+            this.tabs.forEach(function(child) { child.active = (child === tab); });
+        },
+        addTab(tab) {
+            this.tabs.push(tab);
+            if (tab.active == true || !this.current) {
+                this.selectTab(tab);
+            }
+        },
+        removeTab(tab) {
+            const index = this.tabs.indexOf(tab);
+            this.tabs.splice(index, 1);
+            this.$el.removeChild(tab.$el);
+            tab.removeTab();
+            let next = this.tabs.length > 0 ? this.tabs[0] : null;
+            this.selectTab(next);
+        }
+    }
+};
+</script>
+
+<style scoped>
+.tab-container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+.tab-list {
+    list-style-type: none;
+    border-bottom: 2px solid #666;
+    margin: 0;
+    padding: 0;
+    padding-left: 2px;
+    flex: 0 1 24px;
+}
+
+.tab-list li {
+    display: inline-block;
+    min-width: 50px;
+    min-height: 20px;
+    height: calc(100% - 2px);
+    margin-right: 2px;
+    margin-top: 2px;
+    vertical-align: top;
+    padding: 4px 8px 2px 4px;
+    border-radius: 3px 3px 0 0;
+}
+
+.tab-list li.active {
+    color: #eee;
+    background-color: #777;
+    border-bottom: 0;
+}
+
+.tab-list li:hover:not(.active) {
+    background-color: #555;
+    color: #ddd;
+}
+
+.tab-content {
+    flex: 1 1 auto;
+    display: flex;
+}
+
+</style>
---
+++ editor/src/components/widgets/tab-set/tab.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-bind:class="{'active': active}">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'tab',
+    data() {
+        return {
+            active: false,
+        };
+    },
+    props: {
+        title: {
+            required: true,
+            type: String,
+        },
+        isActive: {
+            required: false,
+            type: Boolean,
+            default: false,
+        }
+    },
+
+    methods: {
+        removeTab() {
+            this.$destroy();
+        },
+    },
+    created() {
+        this.active = this.isActive;
+    },
+    mounted() {
+        this.$parent.addTab(this);
+    }
+};
+</script>
+<style scoped>
+div {
+    display: none;
+    background-color: #333;
+    padding-left: 2px;
+    padding-right: 2px;
+    flex: 1 1 auto;
+}
+
+div.active {
+    display: block;
+}
+
+</style>
