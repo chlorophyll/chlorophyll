7492e7084a50dd2f59c786ad34287f400b8578c3
Chris Williamson <c.williamso@gmail.com>
2018-07-25 23:41:15 -0700

    Add message batching

---
+++ common/osc/batch.js
@@ -0,0 +1,30 @@
+/*
+ * Helper class for batching OSC messages. For example:
+ *
+ * bus.batch()
+ *    .message('/foo', [...])
+ *    .message('/bar', [...])
+ *    .send()
+ */
+export default class MessageBatch {
+  constructor(sendCallback) {
+    this.packets = [];
+    this.callback = sendCallback;
+  }
+
+  message(address, args) {
+    assert.ok(String.isString(address));
+    assert.ok(Array.isArray(args));
+
+    this.packets.push({address, args});
+
+    return this;
+  }
+
+  send() {
+    if (this.packets.length === 0)
+      throw new Error('Trying to send empty OSC message batch!');
+
+    return this.callback(this.packets);
+  }
+}
