8d3fb9802febb92d66aa51f5a21b88dc1a3dbf7b
Chris Williamson <c.williamso@gmail.com>
2019-02-10 23:53:00 -0800

    Revert "try obj-file-parser"
    This reverts commit e58ea4610d1388e5d19e6b61fff753f5288741de.

--- editor/src/js/savefile/obj.js
+++ editor/src/js/savefile/obj.js
@@ -1,22 +1,23 @@
 import * as THREE from 'three';
+import 'three-examples/loaders/OBJLoader';
 import * as fs from 'fs';
-import ObjFile from 'obj-file-parser';
 
 import { remote } from 'electron';
 
 export default function importOBJ(filename, done) {
-    return fs.readFile(filename, (err, data) => {
+    const loader = new THREE.OBJLoader();
+
+    fs.readFile(filename, (err, data) => {
         if (err) {
             remote.dialog.showErrorBox('Error importing model', err.message);
             return;
         }
 
-        const obj = new ObjFile(data.toString());
-        const modelData = obj.parse();
-
-        const {faces, vertices} = modelData.models[0];
-        console.log(modelData.models);
-
-        return done(null, modelData);
+        loader.load(
+            'data:application/octet-stream;base64,' + data.toString('base64'),
+            object => done(null, object),
+            xhr => {},
+            err => done(err)
+        );
     });
 }
