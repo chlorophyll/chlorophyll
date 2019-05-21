62bbefe7949551af41bfdac74dbfc1f5380789ce
Chris Williamson <c.williamso@gmail.com>
2019-05-21 01:22:24 -0700

    Add basic UV mapping support
    5d149d5a Fix saving
    a88d8ad1 Fix mapPixels for uv maps
    0263129f Import UV mapping from obj
    ce492935 Add UV mapping

---
+++ common/mapping/uv.ts
@@ -0,0 +1,99 @@
+import {Vector2} from 'three';
+
+import Units from '../units';
+import * as T from '../types';
+
+type UVMode = 'uv2d';
+
+interface UVSettings {
+    dimension: number;
+    uvCoords: Array<THREE.Vector2>;
+}
+
+/*
+ * UVW texture coordinate mappings
+ *
+ * These are actually simpler than projection mapping. The mapping consists of
+ * a simple transformation (translation, rotation, scaling) in 3d space.
+ *
+ * When a pattern is applied, pixel positions can be described in
+ * cartesian, cylindrical, or spherical coordinates.
+ */
+
+export default class UVMapping implements T.PixelMapping {
+    static readonly className = 'uv';
+    readonly className = 'uv';
+    static readonly displayName = 'UV Unwrap';
+    readonly displayName = 'UV Unwrap';
+
+    public settings = {
+        uvCoords: [],
+        dimension: 2
+    };
+
+    static readonly views = [
+        {
+            className: 'uv2d',
+            displayName: '2D UV',
+            coords: [
+                {name: 'x', unit: Units.Distance},
+                {name: 'y', unit: Units.Distance},
+            ],
+            glslType: 'vec2',
+            glslSwizzle: 'xy'
+        }
+    ];
+
+    constructor(attrs) {
+        this.settings = {
+            ...this.settings,
+            ...attrs
+        };
+        if (attrs && attrs.uvCoords)
+            this.settings.uvCoords = attrs.uvCoords.map(uv => new Vector2(...uv));
+
+        console.log(`Init ${this.className} mapping:`, this.settings);
+    }
+
+    getView(): T.MapMode {
+        return UVMapping.views[0];
+    }
+    
+    mapPixels(pixels: Array<T.Pixel>): Array<T.MappedPixel> {
+        return pixels.map(px => {
+            if (!(px.idx in this.settings.uvCoords))
+                return null;
+
+            return {
+                idx: px.idx,
+                pos: this.settings.uvCoords[px.idx]
+            };
+        });
+    }
+
+    /*
+     * Modifying & config methods
+     */
+    setParameters(settings) {
+        this.settings = {
+            ...this.settings,
+            ...settings
+        };
+    }
+
+    /*
+     * Serialization
+     */
+
+    serialize() {
+        return {
+            ...this.settings,
+            uvCoords: this.settings.uvCoords.map(v => v.toArray())
+        };
+    }
+
+    static deserialize(attrs) {
+        return new UVMapping(attrs);
+    }
+}
+
