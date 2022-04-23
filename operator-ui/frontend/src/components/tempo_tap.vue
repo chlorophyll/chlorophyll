b4001c7621714c6d90f6c92ccdd5755a854f0853
Ryan Pearl <rpearl@endofunctor.org>
2022-04-23 11:56:42 -0700

    replace tempo tap component

--- operator-ui/frontend/src/components/tempo_tap.vue
+++ operator-ui/frontend/src/components/tempo_tap.vue
@@ -1,90 +1,102 @@
 <template>
-<v-btn @mousedown="tap">tap</v-btn>
+<button class="square" @mousedown="tap">tap</button>
 </template>
 
 <script>
+const MS_UNTIL_CHAIN_RESET = 2000;
+const SKIPPED_TAP_THRESHOLD_LOW = 1.75;
+const SKIPPED_TAP_THRESHOLD_HIGH = 2.75;
+const TOTAL_TAP_VALUES = 5;
 export default {
     name: 'tempo-tap',
     props: ['value'],
     data() {
         return {
-            startTime: null,
-            beatTimes: [],
-            xsum: 0,
-            xxsum: 0,
-            ysum: 0,
-            yysum: 0,
-            xysum: 0,
+            buttonDown: false,
+            beatMs: 500,
+            tapDurations: new Array(TOTAL_TAP_VALUES).fill(0),
+            lastTapMs: 0,
+            lastTapSkipped: false,
+            tapDurationIndex: 0,
+            tapsInChain: 0,
         };
     },
     watch: {
-        value(v) {
-          if (Math.abs(v-this.tempo) > 10 && this.beatCount > 2) {
-                this.reset();
-            }
+        bpm(v) {
+            this.$emit('input', v);
         },
-        tempo() {
-            this.$emit('input', this.tempo);
+        value(v) {
+            if (v !== this.bpm) {
+                this.resetTapChain(new Date().getTime());
+                this.beatMs = 60000/v;
+            }
         },
     },
     computed: {
-        beatCount() {
-            return this.beatTimes.length;
-        },
-        xx() {
-            if (this.beatCount < 2) return 0;
-            return this.beatCount * this.xxsum - this.xsum * this.xsum;
-        },
-        yy() {
-            if (this.beatCount < 2) return 0;
-            return this.beatCount * this.yysum - this.ysum * this.ysum;
-        },
-        xy() {
-            if (this.beatCount < 2) return 0;
-            return this.beatCount * this.xysum - this.xsum * this.ysum;
-        },
-        period() {
-            if (this.beatCount < 2) return 0;
-            return (this.beatCount * this.xysum - this.xsum * this.ysum) / this.xx;
-        },
-        offset() {
-            if (this.beatCount < 2) return 0;
-            return (this.ysum * this.xxsum - this.xsum * this.xysum) / this.xx;
-        },
-        tempo() {
-            if (this.beatCount < 2) return this.value;
-            return (60*1000) / this.period;
+        bpm() {
+            return 60000/this.beatMs;
         },
     },
     methods: {
-        reset() {
-            this.startTime = null;
-            this.beatTimes = [];
-            this.xsum = 0;
-            this.xxsum = 0;
-            this.ysum = 0;
-            this.yysum = 0;
-            this.xysum = 0;
-        },
         tap() {
-            this.countBeat(Date.now());
-        },
-        countBeat(currTime) {
-            if (this.startTime === null) {
-                this.startTime = currTime;
+            const ms = new Date().getTime();
+
+            if (this.lastTapMs + MS_UNTIL_CHAIN_RESET < ms) {
+                this.resetTapChain(ms);
             }
-            const x = this.beatTimes.length;
-            const y = currTime - this.startTime;
 
-            this.beatTimes.push(y);
-
-            this.xsum += x;
-            this.xxsum += x*x;
-            this.ysum += y;
-            this.yysum += y*y;
-            this.xysum += x*y;
+            const newBeatMs = this.doTap(ms);
+            if (newBeatMs !== -1) {
+                this.beatMs = newBeatMs;
+            }
         },
+        resetTapChain(ms) {
+            console.log('reset');
+            this.tapsInChain = 0;
+            this.tapDurationIndex = 0;
+            this.resetMs = ms;
+            for (let i =0 ;i < TOTAL_TAP_VALUES; i++) {
+                this.tapDurations[i] = 0;
+            }
+        },
+
+        getAverageTapDuration(ms) {
+            const count = Math.min(this.tapsInChain - 1, TOTAL_TAP_VALUES);
+            let total = 0;
+            for (let i = 0; i < count; i++) {
+                total += this.tapDurations[i];
+            }
+            return Math.floor(total / count);
+        },
+
+
+        doTap(ms) {
+            this.tapsInChain++;
+            if (this.tapsInChain === 1) {
+                this.lastTapMs = ms;
+                return -1;
+            }
+
+            let duration = ms - this.lastTapMs;
+
+            if (this.tapsInChain > 1 && !this.lastTapSkipped && duration > this.beatMs * SKIPPED_TAP_THRESHOLD_LOW && duration < this.beatMs * SKIPPED_TAP_THRESHOLD_HIGH) {
+                duration = Math.floor(duration * 0.5);
+                this.lastTapSkipped = true;
+            } else {
+                this.lastTapSkipped = false;
+            }
+
+            this.tapDurations[this.tapDurationIndex] = duration;
+            this.tapDurationIndex++;
+            if (this.tapDurationIndex === this.tapDurations.length) {
+                this.tapDurationIndex = 0;
+            }
+
+
+            const newBeatMs = this.getAverageTapDuration(ms);
+            this.lastTapMs = ms;
+            return newBeatMs;
+        }
     },
 };
 </script>
-
